558dcf21c837df11c7c9b9e722f279f67334bbbc
Florian <falkentavio@hotmail.de>
2019-09-13 13:27:32 +0200

    Update gtm.js
    Add checkout steps

--- gtm.js
+++ gtm.js
@@ -7,8 +7,28 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-5DGSPZ5');
 
+function checkoutStep(step) {
+dataLayer.push({
+  'event': 'checkout',
+  'ecommerce': {
+    'checkout': {
+      'actionField': {'step': step}
+    }
+  }
+})
+}
 
 if (window.eComEventTarget) {
+  window.eComEventTarget.addEventListner('pageview', function (event) {
+    var url = event.detail.url
+    if (url === '/cart') checkoutStep(1)
+    if (url === '/checkout/personal-data') checkoutStep(2)
+    if (url === '/checkout/shipping') checkoutStep(3)
+    if (url === '/checkout/payment') checkoutStep(4)
+    if (url === '/checkout/confirmation') checkoutStep(5)
+    if (url === '/o') checkoutStep(6)
+  })
+  
   window.eComEventTarget.addEventListener('product', function (event) {
     const product = event.detail.product
     dataLayer.push({
